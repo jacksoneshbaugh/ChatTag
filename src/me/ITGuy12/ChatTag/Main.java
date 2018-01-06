5f8392de5c0a7dd9413652a67ab8e127f1473ea4
JavaGuy12 <itguy12games@gmail.com>
2018-01-06 18:45:22 -0500

    Changed ChatColor

--- src/me/ITGuy12/ChatTag/Main.java
+++ src/me/ITGuy12/ChatTag/Main.java
@@ -1,14 +1,13 @@
 package me.ITGuy12.ChatTag;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Bukkit; 
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin implements Listener{
 	
